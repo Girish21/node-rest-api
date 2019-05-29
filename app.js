c22e48fb473964e26969ce46a301371f1fce6faf
Girish <girishkumarv21@gmail.com>
2019-05-29 19:10:31 +0530

    added CORS handlers

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const routes = require("./routes/feed");
+
+app.use(bodyParser.json({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use("/feed", routes);
 
--- controller/feed.js
+++ controller/feed.js
@@ -1,3 +1,17 @@
 exports.getPosts = (req, res, next) => {
   return res.status(200).json({ message: "Hello" });
 };
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  return res.status(201).json({
+    message: "Post created",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content
+    }
+  });
+};
